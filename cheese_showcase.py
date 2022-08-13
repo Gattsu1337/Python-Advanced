5b62e44289353e963dd54892bf3c3f0072376294
Dimitar Velikov <102250347+Gattsu1337@users.noreply.github.com>
2022-08-13 08:45:49 +0300

    Add files via upload

---
+++ cheese_showcase.py
@@ -0,0 +1,20 @@
+def sorting_cheeses(**kwargs):
+    result = []
+    sorted_cheeses = sorted(
+        kwargs.items(),
+        key=lambda x: (-len(x[1]), x[0]),
+    )
+
+    for name, pieces in sorted_cheeses:
+        result.append(name)
+        result.extend(sorted(pieces, reverse=True))
+
+    return '\n'.join([str(x) for x in result])
+
+print(
+    sorting_cheeses(
+        Parmesan=[102, 120, 135],
+        Camembert=[100, 100, 105, 500, 430],
+        Mozzarella=[50, 125],
+    )
+)
